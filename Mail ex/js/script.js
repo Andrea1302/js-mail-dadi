d60a61cf972f2fda0fc84af746e32329da0531de
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-26 15:03:00 +0200

    Aggiunto inner html sulle possibilità

--- Mail ex/js/script.js
+++ Mail ex/js/script.js
@@ -7,11 +7,24 @@
 // Creo array con le mail che potranno accedere 
 
 const listaMail = ["io@gmail.com", "andrea@gmail.com", "scarpandrea.1996@hotmail.com"];
-console.log(listaMail[2]);
+
+let festaJs = document.getElementById("festa");
+
+
 
 
 // Chiedo all utente di inserire la sua mail 
 let inserisciMail = prompt("inserisci qui la tua mail");
 // Se la sua mail corrisponde a una mail presente nella array allora stamperò l esito positivo dell accesso 
+for ( i=0; i < listaMail.length; i++ ) {
+    if ( inserisciMail === listaMail[i] ){
+        console.log("ci sei");
+        festaJs.innerHTML= `Complimenti sei inserito nella lista !`
 
-// Se la sua mail non combacia con quella presente nella array allora stamperò l esito negativo dell accesso 
+        // Se la sua mail non combacia con quella presente nella array allora stamperò l esito negativo dell accesso 
+        
+    } else {
+        console.log("non ci sei");
+        festaJs.innerHTML=`Purtroppo non sei inserito nella lista`
+    }
+}
